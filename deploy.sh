242c1dc9d288b8d777cbe39f91d7a96532b3e19c
Anoop Vijayan <anoopvijayan@outlook.com>
2025-06-10 10:40:01 +0200

    🚀 Deploy update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ COMMIT_MSG=${1:-"🚀 Deploy update"}
 echo "📤 Committing and pushing code to GitHub: $REPO_DIR -> branch $BRANCH"
 
 # Git operations
+# TODO: Add a check to see if the branch is already up to date
 git add .
 git commit -m "$COMMIT_MSG"
 git push origin "$BRANCH"
